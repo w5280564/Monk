11285f84b1cc50d16d44d6995387718ca777f39d
ouyang <675827586@qq.com>
2021-11-24 18:29:05 +0800

    haah

--- app/src/main/java/com/qingbo/monk/login/activity/LoginActivity.java
+++ app/src/main/java/com/qingbo/monk/login/activity/LoginActivity.java
@@ -54,8 +54,6 @@ public class LoginActivity extends BaseActivity{
         baseMap.put("equipmentName", android.os.Build.BRAND + "" + android.os.Build.MODEL);
         baseMap.put("meid", StringUtil.getIMEI());
         baseMap.put("version", android.os.Build.VERSION.RELEASE);
-
-
         HttpSender sender = new HttpSender(HttpUrl.accountLogin, "账号密码登录", baseMap,
                 new MyOnHttpResListener() {
                     @Override
